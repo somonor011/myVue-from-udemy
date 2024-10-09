e965ff9c54bab929a1ad748c6cd91ba580347c05
somonor011 <somonorhong011@gmail.com>
2024-10-09 16:29:53 +0700

    commit1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to SomonorWebsite.org" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import PAGES from './components/new-one.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    // eslint-disable-next-line vue/no-unused-components
+    PAGES,
   }
 }
 </script>
+<!--
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+-->
---
+++ src/components/new-one.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>My frist vue project</h1>
+</template>
+
+<script>
+    export default {
+        name : "PAGES"
+    }
+</script>
+<style scoped></style>

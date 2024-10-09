eb4e161d5909bd8d97dd9c484713659730d991dc
somonor011 <somonorhong011@gmail.com>
2024-10-09 16:56:38 +0700

    commit2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to SomonorWebsite.org" />
+  <PAGES></PAGES>
 </template>
 
 <script>
@@ -16,16 +17,14 @@ export default {
   }
 }
 </script>
-<!--
-  <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  </style>
 
--->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/components/new-one.vue
+++ src/components/new-one.vue
@@ -1,10 +1,30 @@
 <template>
-    <h1>My frist vue project</h1>
+    <!-- {{ 2+2 }} refers to calculate 2+2 so result = My 4 vue projcet -->
+    <h1>My {{ 2 + 2 }} vue project</h1>
+    <!-- Display length of character -->
+    <h1>Length of Hello = {{ "Hello".length }}</h1>
+    <h1>My email : {{ email = "aaa@gmail.com"}}</h1> <!-- we can asign value after property so old value can't display-->
+    <h1>My name is {{ myName() }}</h1>
 </template>
 
 <script>
-    export default {
-        name : "PAGES"
-    }
+export default {
+    name: "PAGES", // name is property to call in file App.vue to define components
+    data() {
+        return {
+            email: 'abc@gmail.com ',
+            // And we can use callback function
+            myName: function () {
+                return "Somonor"
+            }
+        }
+    },
+    
+
+}
 </script>
-<style scoped></style>
+<style scoped>
+h1 {
+    color: rebeccapurple;
+}
+</style>
